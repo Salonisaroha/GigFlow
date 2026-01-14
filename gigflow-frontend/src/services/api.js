75289011e482f5ddb333a2e84e500873181725e9
Saloni Saroha <salonisaroha5@gmail.com>
2026-01-14 21:55:44 +0530

    commit

--- gigflow-frontend/src/services/api.js
+++ gigflow-frontend/src/services/api.js
@@ -1,17 +1,9 @@
 import axios from "axios";
 
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-//     withCredentials: true,
-// });
-
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api"
-
 const api = axios.create({
-  baseURL: API_URL?.startsWith("http")
-    ? API_URL
-    : `https://${API_URL}`,
-  withCredentials: true,
+    baseURL: "https://gigflow-backend-production-07a5.up.railway.app/api" || "http://localhost:5000/api",
+    withCredentials: true,
 });
+
 
 export default api;
